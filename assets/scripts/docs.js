3dc42bb2f91cc132eb65e73b0492f45e446fb8fa
Caleb Evans <caleb@calebevans.me>
2016-12-03 17:18:55 -0800

    Fix live demos that draw images

--- assets/scripts/docs.js
+++ assets/scripts/docs.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
 var $docsSection = $('#docs-section');
 // The pattern to replace jQuery canvas selectors
 var canvasSelectorPattern = /\$\(['"](.*?)canvas(.*?)\1\)/gi;
+// The pattern to replace image paths
+var imagePathPattern = /(images\/(?:\w+)\.jpg)/gi;
+// The pattern to detect if code runs asynchronous functions
+var asyncPattern = /drawImage|createPattern|(?:type: (['"])image\1)/gi;
 
 // Add anchor link to every subsection for easy access later
 $docsSection.find('h3').each(function () {
@@ -23,11 +27,14 @@ $docsSection.find('div.highlighter-rouge').each(function () {
     // canvas has been drawn on
     var demoImageBlank = $demoCanvas[0].toDataURL();
     var code = $codeBlock.text();
+    // Modify any image paths to point to the real images directory
+    code = code.replace(imagePathPattern, '/jcanvas/assets/$1');
     // Inject canvas jQuery element into demo code
     code = code.replace(canvasSelectorPattern, '$demoCanvas');
     new Function(['$demoCanvas'], code)($demoCanvas);
-    // If something was drawn on demo canvas, append it below code block
-    if ($demoCanvas[0].toDataURL() !== demoImageBlank) {
+    // If something was drawn on demo canvas (or if code draws asynchronously),
+    // append canvas below code block
+    if ($demoCanvas[0].toDataURL() !== demoImageBlank || asyncPattern.test(code)) {
       $codeBlock.after($demoCanvas);
       $codeBlock.addClass('has-demo');
     }
