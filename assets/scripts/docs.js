840096117a543f1451771d78b707cce664798c5c
Caleb Evans <caleb@calebevans.me>
2016-12-06 12:50:13 -0800

    Preserve sandbox state of current page in docs
    Previously, the session state of the current page would be overwritten
    when spawning a new page to try code in the sandbox.

--- assets/scripts/docs.js
+++ assets/scripts/docs.js
@@ -81,13 +81,23 @@ $docsSection.on('click', '.demo-rerun', function () {
 $docsSection.on('click', '.try-in-sandbox', function () {
   // Retrieve code for this example from neighboring <pre>
   var code = $(this).next().text();
-  // Stage this code to appear on the next new sandbox instance
+  // Retrieve the sandbox state of the current page
+  var originalSandboxState = sessionStorage.getItem('jcanvas-sandbox');
+  // Temporarily overwrite the current page's sandbox state with the new sandbox
+  // code so that it gets copied into the sandbox state for the spawned page
   sessionStorage.setItem('jcanvas-sandbox', JSON.stringify({
     code: code
   }));
-  // window.open copies the session data from the current page into the session
-  // storage for the new page
+  // window.open copies the session data for the current page into the session
+  // storage for the spawned page
   window.open('/jcanvas/sandbox/');
+  // Restore the original sandbox state for the current page (this should not
+  // affect the sandbox state of the spawned page)
+  if (originalSandboxState !== null) {
+    sessionStorage.setItem('jcanvas-sandbox', originalSandboxState);
+  } else {
+    sessionStorage.removeItem('jcanvas-sandbox');
+  }
 });
 
 });
