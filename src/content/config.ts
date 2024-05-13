820e85235aa4e6d684650c3c396336f2c7092daa
Caleb Evans <caleb@calebevans.me>
2024-05-12 19:52:16 -0700

    Fix routing for Plugins documentation

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,20 @@
 // Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
 import { docsSchema } from '@astrojs/starlight/schema';
+import { defineCollection, z } from 'astro:content';
 
 // Define a `type` and `schema` for each collection
 const galleriesCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-      title: z.string(),
-      author: z.string(),
-      direct_url: z.string(),
-      github_url: z.string().optional(),
-    })
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    direct_url: z.string(),
+    github_url: z.string().optional()
+  })
 });
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   galleries: galleriesCollection,
   docs: defineCollection({ schema: docsSchema() }),
+  plugins: defineCollection({ schema: docsSchema() })
 };
