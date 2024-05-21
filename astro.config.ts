638bc19c189cd616f93f278c15220a086cd6f44d
Caleb Evans <caleb@calebevans.me>
2024-05-20 18:45:49 -0700

    Properly set site in Astro config
    It should be the final URL, not the local one.

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://localhost:4321/',
+  site: 'https://projects.calebevans.me',
   base: '/jcanvas/',
   integrations: [
     astroExpressiveCode({
