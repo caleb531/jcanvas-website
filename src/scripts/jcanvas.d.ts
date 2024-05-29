33ef1b96959d35a2ea6edb881df0fb6b5e21dafd
Caleb Evans <caleb@calebevans.me>
2024-05-29 16:04:16 -0700

    Remove jcanvas module declarations
    Now that the core jcanvas repository is defining them in its own TS
    declarations file.

--- src/scripts/jcanvas.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'jcanvas';
-declare module 'jcanvas/dist/umd/jcanvas-crescents.min.js';
-declare module 'jcanvas/dist/umd/jcanvas-donuts.min.js';
-declare module 'jcanvas/dist/umd/jcanvas-hearts.min.js';
-declare module 'jcanvas/dist/umd/jcanvas-handles.min.js';
