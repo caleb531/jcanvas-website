7d0d1b2c80ce031bc3eb836b6c07b614176fa8e5
Caleb Evans <caleb@calebevans.me>
2024-05-29 15:58:42 -0700

    Accept autocompletion on Tab key press

--- src/scripts/editor.ts
+++ src/scripts/editor.ts
@@ -1,10 +1,12 @@
 // Source:
 // <https://github.com/RPGillespie6/codemirror-quickstart/blob/master/src/editor.js>
 import {
+  acceptCompletion,
   autocompletion,
   closeBrackets,
   closeBracketsKeymap,
-  completionKeymap
+  completionKeymap,
+  selectedCompletion
 } from '@codemirror/autocomplete';
 import {
   defaultKeymap,
@@ -24,6 +26,7 @@ import {
 import {
   EditorSelection,
   EditorState,
+  Prec,
   type EditorStateConfig,
   type Extension,
   type SelectionRange
@@ -91,73 +94,90 @@ function createEditorState(
         ...defaultKeymap,
         ...historyKeymap,
         ...foldKeymap,
-        ...completionKeymap,
-        // Implement the equivalent of VS Code's "Add Selection To Next Find
-        // Match" command (cmd+d on Mac)
-
-        {
-          key: 'Mod-d',
-          run: (view) => {
-            const code = view.state.doc.toString();
-            const selectionRanges = view.state.selection.ranges;
-            const mainSelection = view.state.selection.main;
-            const cursorAnchor = mainSelection.anchor;
-            const cursorHead = mainSelection.head;
-
-            const selectionLength = Math.abs(cursorHead - cursorAnchor);
-
-            // If selection is empty, do nothing
-            if (selectionLength === 0) {
-              return true;
-            }
-
-            // Reset lastSelection if the number of ranges has changed (i.e., new selection made)
-            if (selectionRanges.length === 1 || !lastSelection) {
-              lastSelection = mainSelection;
-            }
-
-            const selectedCode = code.substring(
-              lastSelection.anchor,
-              lastSelection.head
-            );
-            const cursorOffset = Math.max(
-              lastSelection.anchor,
-              lastSelection.head
-            );
-            let offsetOfNextMatch;
-
-            // Try to find the next match after the current selection
-            offsetOfNextMatch = code.indexOf(selectedCode, cursorOffset);
-
-            // If no match is found, try wrapping around
-            if (offsetOfNextMatch === -1) {
-              offsetOfNextMatch = code.indexOf(selectedCode);
-            }
-
-            // If still no match is found, do nothing
-            if (offsetOfNextMatch === -1) {
-              return true;
-            }
-
-            // Update lastSelection with the new selection
-            lastSelection = EditorSelection.range(
-              offsetOfNextMatch,
-              offsetOfNextMatch + selectionLength
-            );
-
-            // Dispatch the new selection, ensuring we maintain existing
-            // selections
-            view.dispatch({
-              selection: EditorSelection.create([
-                ...selectionRanges,
-                lastSelection
-              ])
-            });
-
-            return true;
-          }
-        }
+        ...completionKeymap
       ]),
+      Prec.highest(
+        keymap.of([
+          // Implement the equivalent of VS Code's "Add Selection To Next Find
+          // Match" command (cmd+d on Mac)
+          {
+            key: 'Mod-d',
+            run: (view) => {
+              const code = view.state.doc.toString();
+              const selectionRanges = view.state.selection.ranges;
+              const mainSelection = view.state.selection.main;
+              const cursorAnchor = mainSelection.anchor;
+              const cursorHead = mainSelection.head;
+
+              const selectionLength = Math.abs(cursorHead - cursorAnchor);
+
+              // If selection is empty, do nothing
+              if (selectionLength === 0) {
+                return true;
+              }
+
+              // Reset lastSelection if the number of ranges has changed (i.e., new selection made)
+              if (selectionRanges.length === 1 || !lastSelection) {
+                lastSelection = mainSelection;
+              }
+
+              const selectedCode = code.substring(
+                lastSelection.anchor,
+                lastSelection.head
+              );
+              const cursorOffset = Math.max(
+                lastSelection.anchor,
+                lastSelection.head
+              );
+              let offsetOfNextMatch;
+
+              // Try to find the next match after the current selection
+              offsetOfNextMatch = code.indexOf(selectedCode, cursorOffset);
+
+              // If no match is found, try wrapping around
+              if (offsetOfNextMatch === -1) {
+                offsetOfNextMatch = code.indexOf(selectedCode);
+              }
+
+              // If still no match is found, do nothing
+              if (offsetOfNextMatch === -1) {
+                return true;
+              }
+
+              // Update lastSelection with the new selection
+              lastSelection = EditorSelection.range(
+                offsetOfNextMatch,
+                offsetOfNextMatch + selectionLength
+              );
+
+              // Dispatch the new selection, ensuring we maintain existing
+              // selections
+              view.dispatch({
+                selection: EditorSelection.create([
+                  ...selectionRanges,
+                  lastSelection
+                ])
+              });
+
+              return true;
+            }
+          },
+          // Accept any selected autocompletion with the Tab key (otherwise,
+          // fall back to indentation / the default behavior)
+          {
+            key: 'Tab',
+            run: (view) => {
+              if (selectedCompletion(view.state)) {
+                acceptCompletion(view);
+                return true;
+              } else {
+                // Fall back to default action (i.e. indenting)
+                return false;
+              }
+            }
+          }
+        ])
+      ),
       javascript(),
       syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
       oneDark,
